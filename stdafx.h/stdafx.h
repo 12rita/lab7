9add490281eaed3c4553991c70b7dc926f2f2d2f
12rita <12rita43@gmail.com>
2016-05-30 00:27:28 +0300

    Create stdafx.h

---
+++ stdafx.h/stdafx.h
@@ -0,0 +1,28 @@
+// stdafx.h: включаемый файл для стандартных системных включаемых файлов 
+// или включаемых файлов для конкретного проекта, которые часто используются, но 
+// не часто изменяются 
+// 
+
+#pragma once 
+
+#include "targetver.h" 
+
+#include <stdio.h> 
+#include <tchar.h> 
+#include <string> 
+#include <stdlib.h> 
+#include <iostream> 
+#include <fstream> 
+#include <sstream> 
+#include <iomanip> 
+#include <conio.h> 
+#include <time.h> 
+#include <memory> 
+#include <vector> 
+#include <algorithm> 
+#include <iterator> 
+#include <thread> 
+#include <mutex> 
+#include <Windows.h> 
+#include <queue> 
+#include <condition_variable> 
